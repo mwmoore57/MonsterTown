92be6a032c54bfbbac51c8da5898c68ef6c799e0
MichaelWMoore <mwmoore808@hotmail.com>
2019-12-21 14:33:36 -0700

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Michael W. Moore on 12/21/19.
+//  Copyright © 2019 BigNerdRanch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
